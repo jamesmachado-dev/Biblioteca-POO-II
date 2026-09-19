7f433813da6248cf342244488fb18bdde35a74e3
juansanchezlon <juan.sanchezl@est.iudigital.edu.co>
2026-09-19 17:42:17 -0500

    Implement constructor and override estaDisponible method
    Added constructor and overridden method for availability.

--- LibroDigital.java
+++ LibroDigital.java
@@ -3,15 +3,28 @@ public class LibroDigital extends Libro {
     private double tamanioMB;
     private String urlDescarga;
 
+    
     public LibroDigital(String isbn, String titulo, String formato, double tamanioMB, String urlDescarga) {
-        super(isbn, titulo); // Herencia
+        super(isbn, titulo);
         this.formato = formato;
         this.tamanioMB = tamanioMB;
         this.urlDescarga = urlDescarga;
     }
 
+    // Aca se cumple el requisito: Sobrescritura de método (@Override) ---
+    @Override
+    public boolean estaDisponible() {
+        return true; 
+    }
+
+
     public void descargar() {
         System.out.println("Descargando libro digital desde: " + urlDescarga);
+    }
+
+  
+    public void descargar(String nombreUsuario) {
+        System.out.println("El usuario " + nombreUsuario + " está descargando el libro en formato " + formato);
     }
 
     public String getFormato() { return formato; }
